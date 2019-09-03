3f527f683fa91a757a5a1ab83f22a44b4fdfe49c
harshmewada <harshmewada056@gmail.com>
2019-09-03 16:00:23 +0530

    Updates

---
+++ precache-manifest.79e52b42fa1ba87376c3aaf9fc8deaca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b83bf8e7a1361498cfe782533e0f93",
+    "url": "/darkpapers/index.html"
+  },
+  {
+    "revision": "6b0588bc654e0c5db0b7",
+    "url": "/darkpapers/static/css/main.c1b59d2f.chunk.css"
+  },
+  {
+    "revision": "8a9571b913593610abee",
+    "url": "/darkpapers/static/js/2.2b92ea45.chunk.js"
+  },
+  {
+    "revision": "6b0588bc654e0c5db0b7",
+    "url": "/darkpapers/static/js/main.2f1bc7d5.chunk.js"
+  },
+  {
+    "revision": "ff3da27c0ac008e14d0d",
+    "url": "/darkpapers/static/js/runtime~main.f0fa64c9.js"
+  },
+  {
+    "revision": "763e2d693310904bf12ebaed07fa2fc8",
+    "url": "/darkpapers/static/media/bg2.763e2d69.png"
+  },
+  {
+    "revision": "716c18906f2c47106867e1b509aa428e",
+    "url": "/darkpapers/static/media/lines.716c1890.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/darkpapers/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92f8525226f71be86e1dfd6f27857f0e",
+    "url": "/darkpapers/static/media/navbg.92f85252.png"
+  }
+]);
